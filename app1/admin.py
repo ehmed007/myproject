55a54d42721d055cd0c23fb03a2d5213ea2451be
Ahmed Rasheed <ehmedmughal@gmail.com>
2023-06-03 13:56:27 +0500

    changed

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
 # Register your models here.
+
+# class contact_data(admin.ModelAdmin):
+#     list_display = ('id','email','address','city','comments')
+
+# class blog_data(admin.ModelAdmin):
+#     list_display = ('title','description')
+
+
+# admin.site.register(contact, contact_data) 
+# admin.site.register(blogs, blog_data)
